86bd9f167fa014357d65b9f0aade72d3ee3445ec
cbastian.g <sebastian.gutierrez@miranetwork.co>
2023-03-14 16:30:14 -0500

    Ajustado

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/crear-juego", (req, res) => res.sendFile(__dirname + "/client/create.h
 // El juego
 app.get("/partida", (req, res) => res.sendFile(__dirname + "/client/game.html"));
 
-app.listen(9091, () => console.log("Listening... on http port 9091"));
+app.listen(port, () => console.log("Listening... on http port" + port));
 app.use(express.static(path.join(__dirname, "public")));
 const websocketServer = require("websocket").server;
 const httpServer = http.createServer();
