3fc1fddc4924fed32d37b47522042234ea2eb991
cbastian-g <sebastian.gutierrez@miranetwork.co>
2023-03-14 20:34:05 -0500

    Ajustes finales

--- public/js/client.js
+++ public/js/client.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
 
     $("#btnCreate").on("click", function () {
-        // window.location.href = "/partida?create=true";
         window.location.href = "/crear-juego?owner=true";
     });
 
-    $("#btnJoin").on("click", function () {
-        let gameId = $('#token_input').val();
-        // window.location.href = "/partida?join=true&gameId=" + gameId;
+    $("#join_form").submit(function (event) {
+        event.preventDefault(); // Evita que la página se recargue
+        let gameId = $("#token_input").val();
+
+        // Validar si el campo está vacío
+        if (gameId.trim() === "") {
+            alert("El código de juego no puede estar vacío.");
+            return;
+        }
+
         window.location.href = "/crear-juego?owner=false&gameId=" + gameId;
     });
 
--- public/js/create.js
+++ public/js/create.js
@@ -5,10 +5,8 @@ const gameId = params.get('gameId');
 let cardBack = 0;
 
 $(document).ready(function () {
-    console.log("OWNER FLAG => " + owner);
-    console.log("gameID FLAG => " + gameId);
-
-    if (owner == false) {
+  
+    if (owner !== 'true') {
       $(".cards_select").css('display', 'none');
     } 
 
@@ -16,16 +14,10 @@ $(document).ready(function () {
         event.preventDefault(); // Evita que la página se recargue
         const nombreUsuario = $("#user_name").val();
         const temaCartas = $("#cards_theme").val();
-        // Aquí debes agregar tu lógica para crear el juego
-        // ...
-        // Después de crear el juego, redirige a otra sección de la página
-
 
         if (owner === 'true') {
-          // console.log('IF => ' + owner);
           window.location.href = "/partida?create=true&username=" + nombreUsuario + "&cardBack=" + temaCartas;
         } else {
-          // console.log('ELSE => ' + owner);
           window.location.href = "/partida?join=true&gameId=" + gameId + "&username=" + nombreUsuario;
         }
       });
